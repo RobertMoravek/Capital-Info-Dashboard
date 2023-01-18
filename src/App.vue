51c0b56d3c6f070a14e1412fa3c41a34c8fb40a7
Robert Morávek <robert.moravek@gmail.com>
2023-01-18 17:31:40 +0100

    changed formatting setup, still not automatically giving .vue files 4 space indentation :/

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,7 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div></div>
 </template>
 
 <style>
