233ea7d8710b59c8c37b0a81b1126aa47a61a357
Robert Morávek <robert.moravek@gmail.com>
2023-01-18 20:44:34 +0100

    added fetch for Capitals, added function to chose random Country from fetched Data, called both on mount, added watcher for the currentCountry

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,76 @@
 <script setup lang="ts">
+///////////////////////////////////////////
+// IMPORTS
+
+import { ref, reactive, onMounted, watch } from 'vue';
+import type { CapitalsAndCountries } from './types/types';
+
+// VARIABLES
+
+let capitalsandCountries: CapitalsAndCountries;
+let alreadyShownCountries: number[] = reactive([]);
+let currentCountryNumber = ref<number | null>(null);
+const showDashboard : boolean = false;
+
+//////////////////////////////////////
+//  FUNCTIONS
+
+// Fetch the Capitals and countries
+async function getData(): Promise<CapitalsAndCountries> {
+    const res = await fetch('https://countriesnow.space/api/v0.1/countries/capital');
+    const finalRes = await res.json();
+    return finalRes.data;
+}
+
+// Chose a random country
+function choseRandomCountry(): void {
+    let randomNumber: number = Math.floor(Math.random() * capitalsandCountries.length);
+
+    // If it's already in alreadyShownCountries, try again
+    if (alreadyShownCountries.includes(randomNumber) && alreadyShownCountries.length < capitalsandCountries.length) {
+        choseRandomCountry();
+    } else if (alreadyShownCountries.includes(randomNumber) && alreadyShownCountries.length >= capitalsandCountries.length) {
+        alreadyShownCountries = [];
+        alreadyShownCountries.push(randomNumber);
+        // console.log('else if', alreadyShownCountries);
+        currentCountryNumber.value = randomNumber;
+    } else {
+        alreadyShownCountries.push(randomNumber);
+        // console.log('else', alreadyShownCountries);
+        currentCountryNumber.value = randomNumber;
+
+    }
+}
+
+//////////////////////////////////////////
+// CALLING THE FUNCTIONS
+
+// On mount...
+onMounted(async () => {
+    // ...get the list of Capitals and Countries
+    capitalsandCountries = await getData();
+
+    // Chose a random country out of it
+    choseRandomCountry();
+
+    setInterval(choseRandomCountry, 2000);
+});
+
+// Watch for a change in the currentCountryNumber
+watch(currentCountryNumber, (newNum: number | null, oldNum: number | null) => {
+    if (oldNum === null ) {
+        // Change props -> triggers API call in Components
+        // Wait X amount of time
+        // change showDashboard to true
+    } else {
+        // change showDashboard to false
+        //  Change props -> triggers API call in Components
+        // Wait X amount of time
+        // change showDashboard to true
+    }
+});
+
+
 </script>
 
 <template>
